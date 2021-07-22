dc7b97b3420b537a1fab873d12bbc5c44b8634a0
work <work@DESKTOP-M6MHK1V>
2021-07-22 13:06:24 +0800

    Nitai did option 3

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -188,6 +188,7 @@ public class ResourceCentre {
 		System.out.println("Chromebook added");
 	}
 	
+//Nitai
 	//================================= Option 3 Loan an item (CRUD - Update) =================================
 	public static boolean doLoanCamcorder(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
 		
@@ -218,15 +219,39 @@ public class ResourceCentre {
 		}
 	}
 	
-	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
-		// write your code here
-		return true;
-	}
-	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
-		// write your code here
+	//Nitai
+		public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
+			// write your code here
+			boolean isLoaned = false;
+
+			for (int i = 0; i < chromebookList.size(); i++) {
+				if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
+						&& chromebookList.get(i).getIsAvailable() == true) {
+					
+					chromebookList.get(i).setIsAvailable(false);
+					chromebookList.get(i).setDueDate(dueDate);
+					
+					isLoaned = true;
+					
+				}
+			}
+			return isLoaned;
+		}
+	//Nitai
+		public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
+			// write your code here
+			ResourceCentre.viewAllChromebook(chromebookList);
+			String tag = Helper.readString("Enter asset tag > ");
+			String due = Helper.readString("Enter due date > ");
+			Boolean isLoaned =doLoanChromebook(chromebookList, tag, due);
+			if (isLoaned == false) {
+				System.out.println("Invalid asset tag");
+			} else {
+				System.out.println("Chromebook " + tag + " loaned out");
+			}
+			
+		}
 		
-		
-	}
 	
 	//================================= Option 4 Return an item (CRUD - Update)=================================
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
